5f520897e0eaf7d0035763a03dd9a9b0507f9707
Abid <abidaliawan@rocketmail.com>
2021-07-27 17:08:54 +0000

    model file

--- ml.py
+++ ml.py
@@ -1,3 +1,7 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sn")
+nlp = spacy.load("en_core_web_sm")
+doc = nlp ("Apple buys U.K startup for $1 billion")
+
+for ent in doc.ents:
+    print(ent.text,ent.label_)
